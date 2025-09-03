bddb63b16fca7f29f4194eae5b248b80edc576aa
rchen0714 <rc071404@bu.edu>
2025-09-03 16:32:39 -0400

    added comments to settings.py

--- cs412/settings.py
+++ cs412/settings.py
@@ -114,6 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/' # note: no leading slash!
 
