479e2fe7529ee35755efc0da6a4f434431b4d220
rchen0714 <125341510+rchen0714@users.noreply.github.com>
2025-09-26 18:38:22 -0400

    debugged and added to the stylesheet

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ProfileListView, ProfileDetailView
 
 urlpatterns = [
-    path('', ProfileListView.as_view(), name='show_all'),
-    path('show_all_profiles/', ProfileListView.as_view(), name='show_all'),
-    path('profile/<int:pk>', ProfileDetailView.as_view(), name='profile'),
+    path('', ProfileListView.as_view(), name='show_all_profiles'),
+    path('show_all_profiles/', ProfileListView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='show_profile'),
 ]
