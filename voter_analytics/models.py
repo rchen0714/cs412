6e822f628bfb0543db18faf5e0068bb2bbf42ba7
rchen0714 <125341510+rchen0714@users.noreply.github.com>
2025-10-29 19:31:52 -0400

    finished task 1 and set up the model and loaded all the data

--- voter_analytics/models.py
+++ voter_analytics/models.py
@@ -1,3 +1,100 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Voter(models.Model):
+    '''
+    Store/represent the data for registered voters in the town of Newton, MA.
+    Voter ID,First Name,Last Name, Street Number, Street Name, Apt Number
+    Zip Code, DOB, Registration Date, Party Aff, Precinct number
+    '''
+    # unique identifier
+    voter_id = models.CharField(max_length=20, unique=True)
+
+    first_name = models.TextField()
+    last_name = models.TextField()
+
+    residential_street_number = models.CharField(max_length=10)
+    residential_street_name = models.CharField(max_length=100)
+    residential_apartment_number = models.CharField(max_length=10, blank=True, null=True)
+    residential_zip_code = models.CharField(max_length=10)
+
+    date_of_birth = models.DateField()
+    date_of_registration = models.DateField()
+
+    party_affiliation = models.CharField(max_length=2)
+    precinct_number = models.CharField(max_length=2)
+
+    v20state = models.BooleanField(default=False)
+    v21town = models.BooleanField(default=False)
+    v21primary = models.BooleanField(default=False)
+    v22general = models.BooleanField(default=False)
+    v23town = models.BooleanField(default=False)
+
+    voter_score = models.IntegerField()
+
+
+    def __str__(self):
+        '''Return a string representation of an instance of a voter from this model.'''
+
+        voter_name = f"{self.first_name} {self.last_name}"
+        addr = f"{self.residential_street_number} {self.residential_street_name}"
+        if self.residential_apartment_number:
+            addr += f" Apt {self.residential_apartment_number}"
+
+        voter_info = f"{voter_name}: ({self.voter_id}) - {addr}, {self.residential_zip_code}"
+        return voter_info
+    
+def load_data():
+    '''Function to load data records from CSV file into Django model instances.'''
+
+    # delete existing records to prevent duplicates:
+    Voter.objects.all().delete()
+
+    filename = '/Users/rubychen/Desktop/django/newton_voters.csv'
+    f = open(filename)
+    f.readline() # discard headers
+
+    for line in f:
+        fields = line.strip().split(',')
+
+        try:
+            # create a new instance of Voter object with this record from CSV
+            voter = Voter(
+                voter_id = fields[0],
+                last_name = fields[1],
+                first_name = fields[2],
+                residential_street_number = fields[3],
+                residential_street_name = fields[4],
+                residential_apartment_number = fields[5],
+                residential_zip_code = fields[6], 
+                date_of_birth = datetime.strptime(fields[7], "%Y-%m-%d").date(),
+                date_of_registration = datetime.strptime(fields[8], "%Y-%m-%d").date(),
+                party_affiliation = fields[9].strip(),
+                precinct_number = fields[10],
+                v20state = fields[11].strip().upper() == 'TRUE',
+                v21town = fields[12].strip().upper() == 'TRUE',
+                v21primary = fields[13].strip().upper() == 'TRUE',
+                v22general = fields[14].strip().upper() == 'TRUE',
+                v23town = fields[15].strip().upper() == 'TRUE',
+                voter_score = fields[16],
+            )
+
+            # save the instance to the database
+            voter.save()
+            print(f'Created a new voter: {voter}')
+        
+        except Exception as e:
+            print(f"Skipped: {fields} error: {type(e).__name__} - {e}")
+
+    print(f'Done. Created {len(Voter.objects.all())} Voters.')
+
+
+
+
+    
+
+
+
+
--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -1,3 +1,113 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . models import Result, Voter
+import plotly
+import plotly.graph_objs as go
+
+class VoterListView(ListView):
+    '''View to list voter records.'''
+
+    model = Voter
+    template_name = 'voter_analytics/voters.html'
+    context_object_name = 'voters'
+    paginate_by = 100
+    
+    def get_queryset(self):
+        
+        # start with entire queryset
+        voters = super().get_queryset().order_by('last_name', 'first_name')
+ 
+ 
+        # Filtering fields 
+
+        # filter by party affiliation
+        if 'party' in self.request.GET:
+            party = self.request.GET['party']
+            if party:
+                voters = voters.filter(party_affiliation__iexact=party)
+
+        # filter by minimum date of birth (year)
+        if 'min_year' in self.request.GET:
+            min_year = self.request.GET['min_year']
+            if min_year:
+                voters = voters.filter(date_of_birth__year__gte=min_year)
+
+        # filter by maximum date of birth (year)
+        if 'max_year' in self.request.GET:
+            max_year = self.request.GET['max_year']
+            if max_year:
+                voters = voters.filter(date_of_birth__year__lte=max_year)
+                
+        # filter by voter score
+        if 'score' in self.request.GET:
+            score = self.request.GET['score']
+            if score:
+                voters = voters.filter(voter_score=score)
+
+
+        # filter by election participation
+        if 'v20state' in self.request.GET:
+            voters = voters.filter(v20state=True)
+        if 'v21town' in self.request.GET:
+            voters = voters.filter(v21town=True)
+        if 'v21primary' in self.request.GET:
+            voters = voters.filter(v21primary=True)
+        if 'v22general' in self.request.GET:
+            voters = voters.filter(v22general=True)
+        if 'v23town' in self.request.GET:
+            voters = voters.filter(v23town=True)
+
+        return voters
+    
+ 
+class ResultDetailView(DetailView):
+    '''View to show detail page for one result.'''
+ 
+ 
+    template_name = 'marathon_analytics/result_detail.html'
+    model = Result
+    context_object_name = 'r'
+ 
+ 
+    def get_context_data(self, **kwargs) :
+        '''
+        Provide context variables for use in template
+        '''
+        # start with superclass context
+        context = super().get_context_data(**kwargs)
+        r = context['r']
+ 
+        # create graph of first half/second half as pie chart:
+        x = ['first half', 'second half']
+        first_half_seconds = (r.time_half1.hour * 60 + r.time_half1.minute) * 60 + r.time_half1.second
+        second_half_seconds = (r.time_half2.hour * 60 + r.time_half2.minute) * 60 + r.time_half2.second
+        y = [first_half_seconds , second_half_seconds]
+        
+        # generate the Pie chart
+        fig = go.Pie(labels=x, values=y) 
+        title_text = f"Half Marathon Splits"
+        # obtain the graph as an HTML div"
+        graph_div_splits = plotly.offline.plot({"data": [fig], 
+                                         "layout_title_text": title_text,
+                                         }, 
+                                         auto_open=False, 
+                                         output_type="div")
+        # send div as template context variable
+        context['graph_div_splits'] = graph_div_splits
+ 
+		# create graph of runners who passed/passed by
+        x= [f'Runners Passed by {r.first_name}', f'Runners who Passed {r.first_name}']
+        y = [r.get_runners_passed(), r.get_runners_passed_by()]
+        
+        fig = go.Bar(x=x, y=y)
+        title_text = f"Runners Passed/Passed By"
+        graph_div_passed = plotly.offline.plot({"data": [fig], 
+                                         "layout_title_text": title_text,
+                                         }, auto_open=False, output_type="div",               
+                                         ) 
+        context['graph_div_passed'] = graph_div_passed
+        return context
